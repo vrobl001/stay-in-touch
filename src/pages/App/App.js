2c3e8aaa481d533c2ab1b1a36c32b0ffa6b7b076
Vincent Robles <sylkisgsx@gmail.com>
2020-06-26 15:02:50 -0400

    added navbar component

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom'
 import './App.css';
+
+// Reusable Components
+import Navbar from '../../components/Navbar/Navbar';
 
 class App extends Component {
   state = {
@@ -8,6 +12,7 @@ class App extends Component {
   render() {
     return(
       <div className='outer-container'>
+        <Navbar />
         <div className='inner-container'><h1>Test</h1></div>
       </div>
     );
