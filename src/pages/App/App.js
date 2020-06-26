fbfb627db635a1dd24c7d8023e5a73a853d3627f
Vincent Robles <sylkisgsx@gmail.com>
2020-06-26 14:07:29 -0400

    moved files to app page folder

---
+++ src/pages/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+
+  };
+  render() {
+    return(
+      <div className='outer-container'>
+        <div className='inner-container'></div>
+      </div>
+    );
+  }
+}
+
+export default App;
