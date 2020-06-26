fbb9781d2f3e3e3bd86dc59dc840dbd96a4c3423
Vincent Robles <sylkisgsx@gmail.com>
2020-06-26 13:42:54 -0400

    added boiler plate server config

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const logger = require('morgan');
+const serveFavicon = require('serve-favicon');
+const path = require('path');
+
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+
+});
+
+require('dotenv').config();
+require('./config/database');
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(serveFavicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(require('./config/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/messages', require('./routes/api/messages'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+    console.log(`Socket.io is listening on port ${port}`);
+});
