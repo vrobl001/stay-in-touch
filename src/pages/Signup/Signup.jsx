c1289515ea625549eb258dcf582d15daadc38e26
Vincent Robles <sylkisgsx@gmail.com>
2020-06-27 15:37:59 -0400

    signup page loads signup component

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import SignupForm from '../../components/SignupForm/SignupForm';
 import styles from './Signup.module.css';
 
 const Signup = (props) => {
   return (
     <main>
       <div>
-        <h1>Signup Page</h1>
+        <SignupForm {...props} />
       </div>
     </main>
   );
