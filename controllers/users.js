16f4fca31675ccc438bdc6d6dac7a2a63a59d04c
Vincent Robles <sylkisgsx@gmail.com>
2020-07-13 01:52:35 -0400

    changed error message

--- controllers/users.js
+++ controllers/users.js
@@ -3,39 +3,39 @@ const jwt = require('jsonwebtoken');
 const SECRET = process.env.SECRET;
 
 module.exports = {
-    signup,
-    login
+  signup,
+  login,
 };
 
 async function login(req, res) {
-    try {
-        const user = await User.findOne({ email: req.body.email });
-        if (!user) return res.status(400).json({ err: 'bad credentials' });
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (isMatch) {
-                const token = createJWT(user);
-                res.json({ token });
-            } else {
-                return res.status(401).json({ err: 'bad credentials' });
-            }
-        });
-    } catch (err) {
-        res.status(500).json({
-            err: 'this request cannot be completed at this time'
-        });
-    }
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).json({ err: 'Incorrect email/password!' });
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (isMatch) {
+        const token = createJWT(user);
+        res.json({ token });
+      } else {
+        return res.status(401).json({ err: 'Incorrect email/password!' });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      err: 'this request cannot be completed at this time',
+    });
+  }
 }
 
 async function signup(req, res) {
-    try {
-        const user = await User.create(req.body);
-        const token = createJWT(user);
-        res.json({ token });
-    } catch (err) {
-        res.status(400).json(err);
-    }
+  try {
+    const user = await User.create(req.body);
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 }
 
 function createJWT(user) {
-    return jwt.sign({ user }, SECRET, { expiresIn: '24h' });
+  return jwt.sign({ user }, SECRET, { expiresIn: '24h' });
 }
--- src/utils/userService.js
+++ src/utils/userService.js
@@ -3,48 +3,48 @@ import tokenService from './tokenService';
 const BASE_URL = '/api/users/';
 
 function login(creds) {
-    return fetch(BASE_URL + 'login', {
-        method: 'POST',
-        headers: new Headers({ 'Content-type': 'Application/json' }),
-        body: JSON.stringify(creds)
+  return fetch(BASE_URL + 'login', {
+    method: 'POST',
+    headers: new Headers({ 'Content-type': 'Application/json' }),
+    body: JSON.stringify(creds),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Incorrect email/password!');
+      }
     })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('bad credentials');
-            }
-        })
-        .then(({ token }) => tokenService.setToken(token));
+    .then(({ token }) => tokenService.setToken(token));
 }
 
 function logout() {
-    tokenService.removeToken();
+  tokenService.removeToken();
 }
 
 function getUser() {
-    return tokenService.getUserFromToken();
+  return tokenService.getUserFromToken();
 }
 
 function signup(user) {
-    return fetch(BASE_URL + 'signup', {
-        method: 'POST',
-        headers: new Headers({ 'Content-type': 'Application/json' }),
-        body: JSON.stringify(user)
+  return fetch(BASE_URL + 'signup', {
+    method: 'POST',
+    headers: new Headers({ 'Content-type': 'Application/json' }),
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Email Already Taken!');
+      }
     })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('Email Already Taken!');
-            }
-        })
-        .then(({ token }) => tokenService.setToken(token));
+    .then(({ token }) => tokenService.setToken(token));
 }
 
 export default {
-    signup,
-    getUser,
-    logout,
-    login
+  signup,
+  getUser,
+  logout,
+  login,
 };
