347f8c176a2531441dfe1071a0e8aae0db314348
Vincent Robles <sylkisgsx@gmail.com>
2020-07-16 01:52:01 -0400

    added authentication to image route

--- routes/api/images.js
+++ routes/api/images.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 const imgsCtrl = require('../../controllers/images');
 
-router.post('/', imgsCtrl.sendImages);
-router.get('/', imgsCtrl.retrieveImages);
+router.use(require('../../config/auth'));
+
+router.post('/', isAuthenticated, imgsCtrl.sendImages);
+router.get('/', isAuthenticated, imgsCtrl.retrieveImages);
+
+function isAuthenticated(req, res, next) {
+  if (req.user) return next();
+  return res.status(401).json({ msg: 'not authorized' });
+}
 
 module.exports = router;
