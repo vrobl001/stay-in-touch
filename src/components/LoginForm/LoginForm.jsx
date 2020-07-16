772ad4245edfe9acdc88c374b9b66f28cb40aa79
Vincent Robles <sylkisgsx@gmail.com>
2020-07-16 04:12:50 -0400

    refactored to function component with useState

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,41 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import userService from '../../utils/userService';
 import styles from './LoginForm.module.css';
 
-class LoginForm extends Component {
-  state = this.getInitialState();
-  getInitialState() {
-    return {
-      email: '',
-      password: '',
-      error: '',
-    };
-  }
+export default function LoginForm(props) {
+  const [form, setState] = useState({
+    email: '',
+    password: '',
+    error: '',
+  });
 
-  handleChange = (e) => {
-    this.setState({
+  const handleChange = (e) => {
+    setState({
+      ...form,
       error: '',
       [e.target.name]: e.target.value,
     });
   };
 
-  isFormValid = () => {
-    return this.state.email && this.state.password;
-  };
-
-  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!this.isFormValid()) return;
+    if (!isFormValid()) return;
     try {
-      const { email, password } = this.state;
+      const { email, password } = form;
       await userService.login({ email, password });
-      this.setState(this.getInitialState(), () => {
-        this.props.handleSignupOrLogin();
-        this.props.history.push('/');
-      });
+      props.handleSignupOrLogin();
+      props.history.push('/');
     } catch (error) {
-      this.setState({
+      setState({
+        ...form,
         email: '',
         password: '',
         error: error.message,
@@ -43,53 +36,53 @@ class LoginForm extends Component {
     }
   };
 
-  render() {
-    return (
-      <div className={styles.loginForm}>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.handleSubmit}>
-          <fieldset>
-            <legend>Log in to your account</legend>
+  const isFormValid = () => {
+    return form.email && form.password;
+  };
 
-            <div className={styles.inputField}>
-              <i className='material-icons'>email</i>
-              <input
-                name='email'
-                type='email'
-                value={this.state.email}
-                placeholder='Full Name'
-                onChange={this.handleChange}
-                required
-              />
-              <label htmlFor='email'>Email</label>
-            </div>
+  return (
+    <div className={styles.loginForm}>
+      {form.error && <p>{form.error}</p>}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Log in to your account</legend>
 
-            <div className={styles.inputField}>
-              <i className='material-icons'>lock</i>
-              <input
-                id='signupPassword'
-                name='password'
-                type='password'
-                placeholder='Full Name'
-                minLength='8'
-                onChange={this.handleChange}
-                value={this.state.password}
-                required
-              />
-              <label htmlFor='password'>Password</label>
-            </div>
+          <div className={styles.inputField}>
+            <i className='material-icons'>email</i>
+            <input
+              name='email'
+              type='email'
+              value={form.email}
+              placeholder='Full Name'
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor='email'>Email</label>
+          </div>
 
-            <div className={styles.loginButton}>
-              <button disabled={!this.isFormValid()} type='submit'>
-                Log In
-              </button>
-            </div>
-          </fieldset>
-        </form>
-        <Link to='/signup'>Create account</Link>
-      </div>
-    );
-  }
+          <div className={styles.inputField}>
+            <i className='material-icons'>lock</i>
+            <input
+              id='signupPassword'
+              name='password'
+              type='password'
+              placeholder='Full Name'
+              minLength='8'
+              onChange={handleChange}
+              value={form.password}
+              required
+            />
+            <label htmlFor='password'>Password</label>
+          </div>
+
+          <div className={styles.loginButton}>
+            <button disabled={!isFormValid()} type='submit'>
+              Log In
+            </button>
+          </div>
+        </fieldset>
+      </form>
+      <Link to='/signup'>Create account</Link>
+    </div>
+  );
 }
-
-export default LoginForm;
--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,146 +1,123 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import userService from '../../utils/userService';
 import styles from './SignupForm.module.css';
 
-class SignupForm extends Component {
-  state = this.getInitialState();
+export default function SignupForm(props) {
+  const [form, setState] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConf: '',
+    error: '',
+  });
 
-  getInitialState() {
-    return {
-      name: '',
-      email: '',
-      password: '',
-      passwordConf: '',
+  const handleChange = (e) => {
+    setState({
+      ...form,
       error: '',
-    };
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      error: '',
-      ...{ [e.target.name]: e.target.value },
+      [e.target.name]: e.target.value,
     });
   };
 
-  handlePasswordMatch = () => {
-    if (this.state.password.length >= 8 && this.state.passwordConf.length === this.state.password.length) {
-      if (this.state.password !== this.state.passwordConf) {
-        return this.setState({
-          error: 'Passwords do not match!',
+  const handlePasswordMatch = () => {
+    if (form.password.length >= 8 && form.passwordConf.length === form.password.length) {
+      if (form.password !== form.passwordConf) {
+        return setState({
+          ...form,
           passwordConf: '',
+          error: 'Passwords do not match!',
         });
       }
     }
   };
 
-  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!this.isFormValid()) return;
+    if (!isFormValid()) return;
     try {
-      const { name, email, password } = this.state;
+      const { name, email, password } = form;
       await userService.signup({ name, email, password });
-      this.setState(this.getInitialState(), () => {
-        this.props.handleSignupOrLogin();
-        this.props.history.push('/');
-      });
+      props.handleSignupOrLogin();
+      props.history.push('/');
     } catch (error) {
-      this.setState({
-        name: '',
+      setState({
+        ...form,
         email: '',
-        password: '',
-        passwordConf: '',
         error: error.message,
       });
     }
   };
 
-  isFormValid = () => {
-    this.handlePasswordMatch();
-    return (
-      this.state.name &&
-      this.state.email &&
-      this.state.password &&
-      this.state.passwordConf &&
-      this.state.password === this.state.passwordConf
-    );
+  const isFormValid = () => {
+    handlePasswordMatch();
+    return form.name && form.email && form.password && form.passwordConf && form.password === form.passwordConf;
   };
 
-  render() {
-    return (
-      <div className={styles.signupForm}>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.handleSubmit}>
-          <fieldset>
-            <legend>Sign up</legend>
+  return (
+    <div className={styles.signupForm}>
+      {form.error && <p>{form.error}</p>}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Sign up</legend>
 
-            <div className={styles.inputField}>
-              <i className='material-icons'>person</i>
-              <input
-                name='name'
-                type='text'
-                value={this.state.name}
-                placeholder='Name'
-                onChange={this.handleChange}
-                required
-              />
-              <label htmlFor='name'>Name</label>
-            </div>
+          <div className={styles.inputField}>
+            <i className='material-icons'>person</i>
+            <input name='name' type='text' value={form.name} placeholder='Name' onChange={handleChange} required />
+            <label htmlFor='name'>Name</label>
+          </div>
 
-            <div className={styles.inputField}>
-              <i className='material-icons'>email</i>
-              <input
-                name='email'
-                type='email'
-                value={this.state.email}
-                placeholder='Full Name'
-                onChange={this.handleChange}
-                required
-              />
-              <label htmlFor='email'>Email</label>
-            </div>
+          <div className={styles.inputField}>
+            <i className='material-icons'>email</i>
+            <input
+              name='email'
+              type='email'
+              value={form.email}
+              placeholder='Full Name'
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor='email'>Email</label>
+          </div>
 
-            <div className={styles.inputField}>
-              <i className='material-icons'>lock</i>
-              <input
-                id='signupPassword'
-                name='password'
-                type='password'
-                placeholder='Full Name'
-                minLength='8'
-                onChange={this.handleChange}
-                value={this.state.password}
-                required
-              />
-              <label htmlFor='password'>Password</label>
-            </div>
+          <div className={styles.inputField}>
+            <i className='material-icons'>lock</i>
+            <input
+              id='signupPassword'
+              name='password'
+              type='password'
+              placeholder='Full Name'
+              minLength='8'
+              onChange={handleChange}
+              value={form.password}
+              required
+            />
+            <label htmlFor='password'>Password</label>
+          </div>
 
-            <div className={styles.inputField}>
-              <i className='material-icons'>lock</i>
-              <input
-                id='signupPasswordConf'
-                name='passwordConf'
-                type='password'
-                placeholder='Full Name'
-                minLength='8'
-                value={this.state.passwordConf}
-                onChange={this.handleChange}
-                required
-              />
-              <label htmlFor='passwordConf'>Confirm Password</label>
-            </div>
+          <div className={styles.inputField}>
+            <i className='material-icons'>lock</i>
+            <input
+              id='signupPasswordConf'
+              name='passwordConf'
+              type='password'
+              placeholder='Full Name'
+              minLength='8'
+              value={form.passwordConf}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor='passwordConf'>Confirm Password</label>
+          </div>
 
-            <div className={styles.signupButton}>
-              <button disabled={!this.isFormValid()} type='submit'>
-                Submit
-              </button>
-            </div>
-          </fieldset>
-        </form>
-        <Link to='/login'>Already have account</Link>
-      </div>
-    );
-  }
+          <div className={styles.signupButton}>
+            <button disabled={!isFormValid()} type='submit'>
+              Submit
+            </button>
+          </div>
+        </fieldset>
+      </form>
+      <Link to='/login'>Already have account</Link>
+    </div>
+  );
 }
-
-export default SignupForm;
